558b929096301ef837c614e8bbbff1f0f5a862d3
Hallvar Haugdal <hallvar.haugdal@sintef.no>
2022-09-28 11:33:20 +0200

    Deleted file

--- examples/create_topics.py
+++
@@ -1,9 +0,0 @@
-from src.nqkafka.utils import create_topic
-
-
-if __name__ == '__main__':
-    ip = 'localhost'  # socket.gethostbyname(socket.gethostname())
-    port = 40000
-    bootstrap_servers = ip + ':' + str(port)
-
-    create_topic('time', bootstrap_servers=bootstrap_servers, n_samples=50)
